b928137d64eb047d73ebd076c1880a6eee54081a
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-05-17 06:36:46 -0700

    Initial table populating successfully.

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,21 +1,38 @@
 import React from "react";
 import { Table, Container } from "react-bootstrap";
-// import Moment from "react-moment";
 
-function EmpTable(employee) {
+function EmpTable(props) {
   return (
     <Container>
       <Table striped bordered hover variant="dark">
         <thead>
           <tr>
             <th>Picture</th>
-            <th>First Name</th>
             <th>Last Name</th>
+            <th>First Name</th>
             <th>Phone</th>
             <th>E-Mail</th>
             <th>DOB</th>
           </tr>
         </thead>
+        <tbody>
+          {props.employees.map((employee, index) => (
+            <tr key={index}>
+              <td>
+                <img
+                  alt="thumbnail"
+                  className="img-fluid"
+                  src={employee.picture.thumbnail}
+                />
+              </td>
+              <td>{employee.name.last}</td>
+              <td>{employee.name.first}</td>
+              <td>{employee.phone}</td>
+              <td>{employee.email}</td>
+              <td>{new Date(employee.dob.date).toLocaleDateString()}</td>
+            </tr>
+          ))}
+        </tbody>
       </Table>
     </Container>
   );
