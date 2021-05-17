2c02d6c174647b9c6ce6c67a202b692239f76a72
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-05-17 07:01:14 -0700

    added onchange prop to form, no functionality yet

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,13 @@
 import React from "react";
 import {Form, Container} from "react-bootstrap";
 
-function Search() {
+function Search(props) {
   return (
     <Container className="align-items-center">
       <Form.Row className="justify-content-center">
         <Form.Group controlId="searchField" style={{width: "60%"}}>
           <Form.Label className="text-white">Search by Name:</Form.Label>
-          <Form.Control type="text" placeholder="Type name here" className="bg-dark border-warning text-white"/>
+          <Form.Control type="text" onChange={props.inputChange} value={props.value} placeholder="Enter search terms here" className="bg-dark border-warning text-white"/>
         </Form.Group>
       </Form.Row>
     </Container>
