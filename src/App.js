3acd3559b2eb2cccbf0b479ce2df73272ace116b
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-05-17 05:43:38 -0700

    added table to render

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "./components/Header";
 import Wrapper from "./components/Wrapper";
 import Search from "./components/Search";
+import EmpTable from "./components/Table";
 
 function App() {
   return (
     <Wrapper>
       <Header />
       <Search />
+      <EmpTable />
     </Wrapper>
   );
 }
