514ae1ba39aad072f548ad53ed97842fc8f1395f
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-05-17 04:19:12 -0700

    completed header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,15 @@
 import React from "react";
 import {Jumbotron, Container} from "react-bootstrap";
-// import Container from "react-bootstrap/Container";
+import "./headerStyle.css";
 
 function Header() {
   return (
-    <Jumbotron fluid>
-      <Container className="text-center text-monospace">
+    <Jumbotron fluid className="bg-dark bottomLine">
+      <Container className="text-center text-monospace text-white">
         <h1>Employee Finder</h1>
-        <medium className="text-muted">
+        <h5 className="text-warning">
           Welcome, Big Brother. Who are you looking for today?
-        </medium>
+        </h5>
       </Container>
     </Jumbotron>
   );
